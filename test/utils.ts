cd55962b905718c756517383051fe23cf5d53c27
ochikov <ognyan@limechain.tech>
2022-01-19 11:46:50 +0200

    initial contract design

---
+++ test/utils.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+export const zeroAddress = "0x0000000000000000000000000000000000000000";
+export const oneToken = ethers.utils.parseEther("1");
+export const tenTokens = ethers.utils.parseEther("10");
+export const shares = {
+  zero: 0,
+  fifty: 50,
+  hundred: 100,
+  hundredFifty: 150,
+  twoHundred: 200,
+};
+
+export const getTimestamp = async () => {
+  const blockNumber = await ethers.provider.getBlockNumber();
+  const block = await ethers.provider.getBlock(blockNumber);
+  return block.timestamp;
+};

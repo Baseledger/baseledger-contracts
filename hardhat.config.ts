3f387578478de169edf4531586f6a59ad64ff4a3
Stefan Kostic <skostic9242@gmail.com>
2022-03-14 17:58:30 +0100

    PR comments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -44,16 +44,6 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://localhost:8545",
     },
-    rinkeby: {
-      url: "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-      chainId: 4,
-      accounts: [
-        "b7a433ab23c96f64d6a5c29fb9bd6c0fcfdcb0f011fa7e996e4768cff6b7c469",
-        "330912a17ad3767738b1b93d968ded600ddbf72bd34ab4414c318f3456de9ec3",
-        "b4fe46532de49ff6cfcd54bf2e84b474ac970d613fef6e27a6ed75a54ed8c0d2",
-        "8f08f9d354c1a518864c62781cdb1dd7e1d956be72f7c929a24f655b318d5b92",
-      ],
-    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
